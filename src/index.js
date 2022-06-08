f65869f445401d3f6787edb1f0b59943fc123c17
chaddha1 <Himanshu.Chaddha@gartner.com>
2022-06-08 11:34:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Create a react component
+const App = () => {
+  const buttonText = "Click me!";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name :
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
